6684c225d03e37b54d3af4c1a36b450fd23211a5
Mirii Alex Njuguna <145835776+Mirii100@users.noreply.github.com>
2024-11-12 18:29:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vacant_residents_locator_for_lenting
+ vacant house locator
---
+++ android/app/src/main/java/com/example/vacant_residents_locator_for_lenting/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.vacant_residents_locator_for_lenting;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
